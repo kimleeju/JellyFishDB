806c269d0b21e59880ddcc8e55c8bea5b20ef186
Eunji <eunjicious@gmail.com>
2019-10-13 02:16:54 +0900

    deleted

--- src_cpp/perf_result/1_extract.sh
+++ src_cpp/perf_result/1_extract.sh
@@ -2,24 +2,27 @@
 
 skiplists="BlockedSpinSkipList BlockedCVSkipList ConcurrentSkipList JDKSkipList JellyFishSkipList"
 threads="1 2 4"
+ops="put get range_query"
 
 
-ops="1000000"
+num="1000000"
 workloads="uni zipf_1.4"
 
-for wk in $workloads; do
-	srcfile="perf_"$ops"_"$wk".rslt"
-	echo "$srcfile"
-	datafile="perf_"$ops"_"$wk".dat"
-
-	echo "thread $skiplists" > $datafile
-	for th in $threads; do
-		line="$th "
-		for sk in $skiplists; do
-			line=$line`grep $sk $srcfile | grep "run" | awk -v sl="$sk" '$1==sl && $7=='"${th}"' {print $NF, s}'`
-			#grep $sk $srcfile | grep "run" | awk '$7=='"${th}"' && $1=='"${sk}"' {print $1}'
+for op in $ops; do
+	for wk in $workloads; do
+		srcfile="perf_"$op"_"$wk".rslt"
+		echo "$srcfile"
+		datafile="perf_"$op"_"$wk".dat"
+	
+		echo "thread $skiplists" > $datafile
+		for th in $threads; do
+			line="$th "
+			for sk in $skiplists; do
+				line=$line`grep $sk $srcfile | grep "run" | awk -v sl="$sk" '$1==sl && $7=='"${th}"' {print $NF, s}'`
+				#grep $sk $srcfile | grep "run" | awk '$7=='"${th}"' && $1=='"${sk}"' {print $1}'
+			done
+			echo $line >> $datafile
 		done
-		echo $line >> $datafile
+		cat $datafile
 	done
-	cat $datafile
 done
