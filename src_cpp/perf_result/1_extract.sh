d2be4d085da320178c8462c8892a7f7d7b007a47
Eunji <eunjicious@gmail.com>
2019-10-13 00:24:00 +0900

    gnuplot added

--- src_cpp/perf_result/1_extract.sh
+++ src_cpp/perf_result/1_extract.sh
@@ -2,7 +2,14 @@ datafile="ss.dat"
 srcfile="perf_1000000_uni.rslt"
 
 skiplists="BlockedSpinSkipList BlockedCVSkipList ConcurrentSkipList JDKSkipList JellyFishSkipList"
+threads="1 2 4"
 
-for sk in $skiplists; do
-	grep $sk $srcfile | awk '{print $1}'
+echo "thread $skiplists"
+for th in $threads; do
+	line="$th "
+	for sk in $skiplists; do
+		line=$line`grep $sk $srcfile | grep "run" | awk -v sl="$sk" '$1==sl && $7=='"${th}"' {print $NF, s}'`
+		#grep $sk $srcfile | grep "run" | awk '$7=='"${th}"' && $1=='"${sk}"' {print $1}'
+	done
+	echo $line
 done
